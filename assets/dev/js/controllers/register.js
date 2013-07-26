db9c1ea2b3b0256b4d3f55b921b8a121873f598c
Poplava <igor.poplava@gmail.com>
2013-07-26 17:18:55 +0300

    submit method

--- assets/dev/js/controllers/register.js
+++ assets/dev/js/controllers/register.js
@@ -1,5 +1,22 @@
 'use strict';
 
 app.controller('RegisterCtrl', ['$scope', function($scope) {
+    $scope.errors = {
+        form: !1,
+        email: !1,
+        login: !1,
+        firstName: !1,
+        lastName: !1,
+        password: !1,
+        passwordConfirm: !1
+    };
 
+    $scope.submitForm = function() {
+        $scope.errors.form = !0;
+        console.log(JSON.stringify($scope.regData));
+    };
+
+    $scope.validate = function() {
+
+    };
 }]);
