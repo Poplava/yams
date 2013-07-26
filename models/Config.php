bc1ca3483b1975874fd9e0fd1edd83b372b5d900
Bocharov Alexandr <bocharov.alexandr@gmail.com>
2013-07-26 13:25:04 +0300

    db, config

---
+++ models/Config.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Class for getting configs
+**/
+class Config
+{
+    /**
+     * Function gets config by name.yaml using params
+     *
+     * @param $params - array
+     *
+     * @return mixed
+    **/
+    static public function get($name, $params)
+    {
+        
+    }
+}
---
+++ models/DB.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * DB class for connecting, executing queries, etc.
+**/
+class DB
+{
+    public function __construct()
+    {
+        
+    }
+
+    public function query()
+    {
+     
+    }
+
+    public function queryRow()
+    {
+        
+    }
+
+    public function queryColumn()
+    {
+        
+    }
+
+    public function queryScalar()
+    {
+        
+    }
+
+    public function execute()
+    {
+        
+    }
+}
