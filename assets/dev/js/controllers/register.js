fb317c792abddde4161e46fab7152ff76f63de02
Poplava <igor.poplava@gmail.com>
2013-07-26 17:46:48 +0300

    form validation

--- assets/dev/js/controllers/register.js
+++ assets/dev/js/controllers/register.js
@@ -1,6 +1,14 @@
 'use strict';
 
-app.controller('RegisterCtrl', ['$scope', function($scope) {
+app.controller('RegisterCtrl', ['$scope', '$http', function($scope, $http) {
+    $scope.regData = {
+        email: "",
+        login: "",
+        firstName: "",
+        lastName: "",
+        password: "",
+        passwordConfirm: ""
+    };
     $scope.errors = {
         form: !1,
         email: !1,
@@ -12,11 +20,27 @@ app.controller('RegisterCtrl', ['$scope', function($scope) {
     };
 
     $scope.submitForm = function() {
-        $scope.errors.form = !0;
-        console.log(JSON.stringify($scope.regData));
+        if($scope.validate()) {
+            $http.post('/register', $scope.regData).success(function() {
+
+            });
+        }
     };
 
     $scope.validate = function() {
-
+        $scope.errors.form = !1;
+        for(var key in $scope.regData) {
+            if($scope.regData[key]) {
+                $scope.errors[key] = !1;
+            } else {
+                $scope.errors.form = $scope.errors[key] = !0;
+            }
+        }
+        if(!$scope.errors.form && ($scope.regData.password !== $scope.regData.passwordConfirm)) {
+            $scope.errors.form =
+                $scope.errors.password =
+                $scope.errors.passwordConfirm = !0;
+        }
+        return !$scope.errors.form;
     };
 }]);
