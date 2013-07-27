0a2db8e378f5fbafb3331b42f0432847057a4d1c
Poplava <igor.poplava@gmail.com>
2013-07-27 08:55:45 +0300

    registration complete

--- assets/dev/js/controllers/Register.js
+++ assets/dev/js/controllers/Register.js
@@ -1,13 +1,28 @@
 'use strict';
 
 app.controller('RegisterCtrl', ['$scope', '$http', 'Auth', function($scope, $http, Auth) {
+    $scope.regComplete =
+        $scope.regError = !1;
 
     $scope.submitForm = function() {
         if($scope.regUserForm && $scope.regUser.password === $scope.passwordConfirm) {
             Auth.register($scope.regUser, function(res) {
-                console.log(res);
+                if(res.userId > 0) {
+                    console.log(res);
+                    $scope.regComplete = !0;
+                } else {
+                    $scope.formError(res);
+                }
+            }, function(res) {
+                $scope.formError(res);
             });
         }
+    };
+
+    $scope.formError = function(res) {
+        console.log(res);
+        $scope.regError = !0;
+        $scope.regUser.password = $scope.passwordConfirm = '';
     };
 
 }]);
--- assets/dev/js/services/Auth.js
+++ assets/dev/js/services/Auth.js
@@ -2,11 +2,9 @@ app.factory('Auth', ['$http', function($http) {
     return {
         register: function(data, success, error) {
             $http.post('/register', data).success(function(res) {
-                console.log(res);
-                if(typeof success === 'object') success(res);
+                if(typeof success === 'function') success(res);
             }).error(function() {
-                console.log(res);
-                if(typeof error === 'object') error(res);
+                if(typeof error === 'function') error(res);
             });
         }
     };
