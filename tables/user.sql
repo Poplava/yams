db21d8253ce324cd7b49b77cfc3f7711d8ddf0d0
Sergei Mikhalchuk <mikhalchuk@maximagroup.com>
2013-07-31 14:34:06 +0000

    remove login column from user table

--- tables/user.sql
+++ tables/user.sql
@@ -3,12 +3,11 @@ CREATE TABLE user
     userId int not null auto_increment primary key,
     roleId int not null default 0,
     email varchar(255) not null,
-    login varchar(255) not null,
     passwordHash char(32) not null,
     firstName varchar(255) not null,
     lastName varchar(255) not null,
     karma int default 0,
     avatarFile varchar(255),
     registeredOn datetime not null,
-    UNIQUE KEY (login)
+    UNIQUE KEY (email)
 );
